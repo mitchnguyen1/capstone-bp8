010714cc9eeb7f270f32b134eef1cc77634b11a6
Mitchell Nguyen <mitch01nguyen@gmail.com>
2023-05-15 15:47:54 -0500

    adds basic HTML files

---
+++ js/gsap.js
@@ -0,0 +1,11 @@
+gsap.set(".ball", {xPercent: -50, yPercent: -50});
+
+let xTo = gsap.quickTo(".ball", "x", {duration: 0.6, ease: "power3"}),
+    yTo = gsap.quickTo(".ball", "y", {duration: 0.6, ease: "power3"});
+
+window.addEventListener("mousemove", e => {
+  // xTo(e.pageX);
+  // yTo(e.pageY);
+  xTo(e.clientX);
+  yTo(e.clientY);
+});
